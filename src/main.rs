c7bd6a78e03a62122db81e489b3546985356e047
Damien Sedgwick <damienksedgwick@gmial.com>
2024-10-27 18:39:43 +0000

    removed benchmarking as I learned of time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::time::Instant;
+fn main() {
+    let stop_at = 1_000_000;
 
-fn fizz_buzz(stop_at: u32) {
     for i in 1..=stop_at {
         if i % 3 == 0 {
             print!("Fizz");
@@ -15,29 +15,5 @@ fn fizz_buzz(stop_at: u32) {
         }
 
         println!();
-    }
-}
-
-fn run_benchmark(stop_at: u32) -> f64 {
-    let start = Instant::now();
-    fizz_buzz(stop_at);
-    start.elapsed().as_secs_f64()
-}
-
-fn main() {
-    const STOP_AT: u32 = 1_000_000;
-    let seconds = run_benchmark(STOP_AT);
-    println!("\nTime taken: {:.6}s", seconds);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn benchmark_fizz_buzz() {
-        const STOP_AT: u32 = 1_000_000;
-        let seconds = run_benchmark(STOP_AT);
-        println!("\nTest benchmark time: {:.6}s", seconds);
     }
 }
